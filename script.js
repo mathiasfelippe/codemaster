bca43944cf96b17fbe87f2a5cca3b08f9251b654
mathiasfelippe <m4thgo@gmail.com>
2026-03-06 14:40:44 -0300

    Etapa 7 concluída
    Botão de voltar ao topo finalizado

--- script.js
+++ script.js
@@ -142,7 +142,12 @@ sections.forEach((section, index) => {
         });
     });
 
-    // Observa cada seção para aplicaf a animação
+    // Observa cada seção para aplicar a animação
     sections.forEach((section) => observer.observe(section));
 
-    
+    // ========================= BOTÃO DE VOLTAR AO TOPO ====================
+    // Adiciona um evento de clique ao botão de voltar ao topo
+    document.querySelector('.top a').addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Rola suavemente para o topo da página
+    });
